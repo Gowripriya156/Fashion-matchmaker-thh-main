bdcaf4772e84bd74c2c2bd8cb7d18e5eaf21b03d
Gowripriya156 <gpmj2006@gmail.com>
2026-02-14 09:46:20 +0530

    static file added

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-app.use(express.static(path.join(__dirname, '/'))); // Serve static files from root
+app.use(express.static(path.join(__dirname, '..','frontend'))); // Serve static files from root
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..','frontend','index.html'));
