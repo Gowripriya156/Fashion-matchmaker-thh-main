c35090c87faedcb38775a3e7181e44b27c51f656
Gowripriya156 <gpmj2006@gmail.com>
2026-02-14 09:39:58 +0530

    home route added

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, '/'))); // Serve static files from root
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','frontend','index.html'));
+});
+
 // API Proxy Route
 app.post('/api/style', async (req, res) => {
     console.log(`Received API request from ${req.ip}`);
